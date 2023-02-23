33e9fcbf6fbfbffa39ba9a12dfd53adb9ac4a569
ABHI <126139101+matrix-h4cker@users.noreply.github.com>
2023-02-23 11:42:13 +0530

    Add files via upload

---
+++ mtinstall.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#code_by_abhi
+clear
+sleep 1
+echo ""
+echo ""
+echo " 
+
+███╗░░░███╗░█████╗░████████╗██████╗░██╗██╗░░██╗
+████╗░████║██╔══██╗╚══██╔══╝██╔══██╗██║╚██╗██╔╝
+██╔████╔██║███████║░░░██║░░░██████╔╝██║░╚███╔╝░
+██║╚██╔╝██║██╔══██║░░░██║░░░██╔══██╗██║░██╔██╗░
+██║░╚═╝░██║██║░░██║░░░██║░░░██║░░██║██║██╔╝╚██╗
+╚═╝░░░░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝╚═╝░░╚═╝    | lolcat
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "Loading==> " | lolcat
+sleep 1
+clear
+echo "Loading====> " | lolcat
+sleep 1
+clear
+echo "Loading=======> " | lolcat
+sleep 1
+clear
+echo " done " | lolcat
+sleep 1
+clear
+unzip techcochi.zip
+clear
+rm -rf techcochi.zip
+clear
+bash about.sh
+sleep 4
+echo ""
+echo ""
+echo " SUBSCRIBE MY CHANNEL  " | lolcat
+xdg-open https://www.youtube.com/@mtrx77
+echo ""
+sleep 10
+echo " visit my blog "
+echo " set your browser "
+termux-open-url https://apknet161.blogspot.com/
+echo ""
